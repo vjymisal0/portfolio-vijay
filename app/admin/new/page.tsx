75a92a76e7df6328575ae8dd5d206dd02a242a2d
Vijay Misal <misalvijay153@gmail.com>
2026-07-21 22:44:31 +0530

    refactor(admin): remove admin panel functionality and related components

--- app/admin/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostEditor from '@/components/admin/post-editor'
-
-export default function NewPostPage() {
-  return (
-    <div className="px-5 pb-16 pt-4 sm:px-6">
-      <PostEditor />
-    </div>
-  )
-}
