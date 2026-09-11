e352c8e101bfc8163f9abca7c45ffe64867cef65
vjymisal0 <misalvijay153@gmail.com>
2026-09-11 14:53:46 +0530

    fix: add mobile spacing to projects section

--- components/work.tsx
+++ components/work.tsx
@@ -4,7 +4,7 @@ import Education from '@/components/education'
 export default function ProjectsSection() {
   return (
     <section>
-      <div className="container mx-auto max-w-4xl space-y-16 py-10">
+      <div className="container mx-auto max-w-4xl space-y-16 px-6 py-10 lg:px-12">
         <Projects />
         <Education />
       </div>
